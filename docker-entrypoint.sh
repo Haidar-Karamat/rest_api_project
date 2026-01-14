b411057ad3a481fdf21b1019fec4584c1111f88c
Haidar-Karamat <haidarkaramat2005@gmail.com>
2026-01-14 15:09:47 +0530

    render(env_var): trii Commit

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# Retry until DB is reachable
-until flask db upgrade; do
-  echo "Database not ready, retrying in 5s..."
-  sleep 5
-done
+flask db upgrade
 
-# Start Gunicorn
 exec gunicorn --bind 0.0.0.0:80 "app:create_app()"
